bd8e64900b651255f2ae9f4aa7bc7f275babe79d
Ryan Villanueva <ryanvill@gmail.com>
2017-11-01 12:27:14 -0400

    Separated visualizer and config

---
+++ controllers/index.controller.js
@@ -0,0 +1,129 @@
+class IndexController {
+  constructor(){
+    this.CampaignManager = new CampaignManager();
+    this.TransformService = new TransformService();
+    this.PerchUnit = new PerchUnit();
+    this.campaign;
+    this.canvas;
+    this.points = [];
+    this.canvasWidth = window.outerWidth;
+    this.canvasHeight = window.outerHeight;
+
+    this.setupCampaign(sample || {});
+    this.PerchUnit.on('sensing', e => this.handleHubEvent(e));
+
+    for (var i = 0; i < 10; i++){
+      for(var j = 0; j < 10; j++){
+        this.addPoint(i/10, j/10, 20);
+      }
+    }
+  }
+
+  handleHubEvent(e){
+    if(e.raw && typeof e.raw.x === 'number' && typeof e.raw.y === 'number'){
+      this.addPoint(e.raw.x, e.raw.y);
+    }
+  }
+
+  setupCampaign(campaign){
+    this.campaign = campaign;
+    this.campaign.regions = this.campaign.regions || {};
+    this.campaign.transforms = this.campaign.transforms || {};
+    this.campaign.regions.areas = this.campaign.regions.areas || [];
+  }
+
+  addPoint(x, y, life){
+    life = life || 100;
+    var point = {
+      x: x,
+      y: y,
+      totalLife: life,
+      currentLife: life
+    };
+    this.campaign.regions.areas.map((area, a) => {
+      if(this.TransformService.rawPointIsWithinArea(point, area, this.campaign.regions.transforms)){
+        point.regionIndex = a;
+      }
+    })
+    this.points.push(point);
+  }
+
+  agePoints(){
+    this.points = this.points.map(point => {
+      point.currentLife -= 1;
+      return point;
+    })
+    this.points = this.points.filter(point => {
+      return point.currentLife > 0;
+    });
+  }
+
+  areaIsActive(a){
+    var activeLife = 30;
+    for(var i = 0; i < this.points.length; i++){
+      if(
+        this.points[i].regionIndex == a &&
+        this.points[i].totalLife - this.points[i].currentLife < activeLife
+      ){
+        return true;
+      }
+    }
+    return false;
+  }
+
+
+  // Canvas functions
+
+  setup(){
+    this.canvas = createCanvas(this.canvasWidth, this.canvasHeight);
+    this.canvas.parent('canvasContainer');
+  }
+
+  draw(){
+    // Draw
+    this.drawBackground();
+
+    this.campaign.regions.areas.map((area, a) => {
+      this.drawArea(area, this.areaIsActive(a), this.campaign.regions.transforms.width, this.campaign.regions.transforms.height);
+    })
+    this.points.map(point => {
+      this.drawPoint(point);
+    })
+
+    // Update
+    this.agePoints();
+  }
+
+  drawBackground(){
+    noStroke();
+    fill(80, 80, 80);
+    rect(0, 0, this.canvasWidth, this.canvasHeight);
+  }
+
+  drawArea(area, isActive, frameWidth, frameHeight){
+    var scaled = this.TransformService.scaleAreaToCanvas(area, frameWidth, frameHeight, this.canvasWidth, this.canvasHeight);
+    isActive ? fill(252, 63, 63) : fill(255);
+    stroke(1);
+    rect(scaled.x, scaled.y, scaled.width, scaled.height);
+    noStroke();
+    fill(0);
+    textSize(14);
+    textAlign(CENTER, CENTER);
+    text(scaled.name, scaled.x, scaled.y, scaled.width, scaled.height);
+  }
+
+  drawPoint(point){
+    var opacity = Math.floor(point.currentLife/point.totalLife * 100)/100 * 255;
+    noStroke();
+    stroke(255, opacity);
+    fill(252, 63, 63, opacity);
+    ellipse(point.x * this.canvasWidth, point.y * this.canvasHeight, 10, 10);
+  }
+
+  resizeCanvas(width, height){
+    console.log(`Resizing canvas to ${width}px by ${height}px`);
+    this.canvasHeight = height;
+    this.canvasWidth = width;
+    resizeCanvas(width, height);
+  }
+}
--- scripts/transforms.js
+++ scripts/transforms.js
@@ -1,88 +1,112 @@
-function normalizeAreas(areas, transforms){
-  var normalized = [];
+class TransformService {
+  constructor(){
 
-  areas.map(area => {
-    var pushed = {
-      x: area.x,
-      y: area.y,
-      width: area.width,
-      height: area.height,
-      name: area.name
-    }
-    var width = transforms.width || 1;
-    var height = transforms.height || 1;
-    if(width){
-      pushed.x = pushed.x/width;
-      pushed.width = pushed.width/width;
-    }
-    if(height){
-      pushed.y = pushed.y/height;
-      pushed.height = pushed.height/height;
-    }
-    normalized.push(pushed);
-  })
+  }
 
-  return normalized;
-}
+  rawPointIsWithinArea(point, area, transforms){
+    var transformWidth = transforms.width || 1;
+    var transformHeight = transforms.height || 1;
+    var isWithinWidth = point.x > area.x / transformWidth && point.x < (area.x + area.width) / transformWidth;
+    var isWithinHeight = point.y > area.y / transformHeight && point.y < (area.y + area.height) / transformHeight;
+    return isWithinWidth && isWithinHeight;
+  }
 
+  normalizeAreas(areas, transforms){
+    var normalized = [];
 
-function flipX(regions){
-  regions.areas.forEach(area => {
-    area.x = regions.transforms.width - area.x - area.width;
-  });
-  return regions;
-}
+    areas.map(area => {
+      var pushed = {
+        x: area.x,
+        y: area.y,
+        width: area.width,
+        height: area.height,
+        name: area.name
+      }
+      var width = transforms.width || 1;
+      var height = transforms.height || 1;
+      if(width){
+        pushed.x = pushed.x/width;
+        pushed.width = pushed.width/width;
+      }
+      if(height){
+        pushed.y = pushed.y/height;
+        pushed.height = pushed.height/height;
+      }
+      normalized.push(pushed);
+    })
 
-function flipY(regions){
-  regions.areas.forEach(area => {
-    area.y = regions.transforms.height - area.y - area.height;
-  });
-  return regions;
-}
+    return normalized;
+  }
 
+  flipX(regions){
+    regions.areas.forEach(area => {
+      area.x = regions.transforms.width - area.x - area.width;
+    });
+    return regions;
+  }
 
-function rotateClockwise(regions){
-  var normalized = normalizeAreas(regions.areas, regions.transforms);
-  var rotated = normalized.map(area => {
-    var x = 1 - area.y - area.height;
-    var y = area.x;
-    var height = area.width;
-    var width = area.height;
-    area.x = x;
-    area.y = y;
-    area.width = width;
-    area.height = height;
-    return area;
-  });
-  regions.areas = rotated;
-  regions.areas = transformAreas(regions.areas, regions.transforms);
-  regions.areas = roundAreas(regions.areas);
-  return regions;
-}
+  flipY(regions){
+    regions.areas.forEach(area => {
+      area.y = regions.transforms.height - area.y - area.height;
+    });
+    return regions;
+  }
 
-
-function transformAreas(areas, transforms){
-  var transformed = areas.map(area => {
-    if(!transforms){
+  rotateClockwise(regions){
+    var normalized = normalizeAreas(regions.areas, regions.transforms);
+    var rotated = normalized.map(area => {
+      var x = 1 - area.y - area.height;
+      var y = area.x;
+      var height = area.width;
+      var width = area.height;
+      area.x = x;
+      area.y = y;
+      area.width = width;
+      area.height = height;
       return area;
-    }
-    var width = transforms.width || 1;
-    var height = transforms.height || 1;
+    });
+    regions.areas = rotated;
+    regions.areas = transformAreas(regions.areas, regions.transforms);
+    regions.areas = roundAreas(regions.areas);
+    return regions;
+  }
 
-    if(width){
-      area.x = area.x * width;
-      area.width = area.width * width;
+  transformAreas(areas, transforms){
+    var transformed = areas.map(area => {
+      if(!transforms){
+        return area;
+      }
+      var width = transforms.width || 1;
+      var height = transforms.height || 1;
+
+      if(width){
+        area.x = area.x * width;
+        area.width = area.width * width;
+      }
+      if(height){
+        area.y = area.y * height;
+        area.height = area.height * height;
+      }
+      return area;
+    })
+    return transformed;
+  }
+
+  scaleAreaToCanvas(area, frameWidth, frameHeight, canvasWidth, canvasHeight){
+    if(!width || !height){
+      throw Error('Needs width and height to scale canvas area.')
     }
-    if(height){
-      area.y = area.y * height;
-      area.height = area.height * height;
+    var newArea = {
+      name: area.name || 'unnamed',
+      x: area.x / (frameWidth || 1) * canvasWidth,
+      y: area.y / (frameHeight || 1) * canvasHeight,
+      width: area.width / (frameWidth || 1)* canvasWidth,
+      height: area.height / (frameHeight || 1)* canvasHeight
     }
-    return area;
-  })
-  return transformed;
+    return newArea;
+  }
+
 }
-
-// Helper functions
 
 function roundNumber(number, places){
   var mag = Math.pow(10, places || 0);
@@ -97,12 +121,4 @@ function roundAreas(areas){
     area.height = roundNumber(area.height, 3);
     return area;
   })
-}
-
-var transforms = {
-  normalizeAreas: normalizeAreas,
-  flipX: flipX,
-  flipY: flipY,
-  rotateClockwise: rotateClockwise,
-  transformAreas: transformAreas
 }
